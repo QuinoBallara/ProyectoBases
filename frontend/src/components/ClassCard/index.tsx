f4e1b0a9cca80950527c108f0256915fa694b878
Quino <joaquinballara02@gmail.com>
2024-11-18 16:41:14 -0300

    layout done

--- frontend/src/components/ClassCard/index.tsx
+++ frontend/src/components/ClassCard/index.tsx
@@ -1,42 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import {ClassProps} from '../../consts/classProps'
+import { ClassProps } from '../../consts/classProps'
 
 import './styles.scss'
 import Button from '../Button'
+import Dropdown from '../DropdownPolenta'
 
 export const ClassCard = (props: ClassProps) => {
 
-  const handleEdit = (): void => {};
+  const [selectedStudent, setSelectedStudent] = useState<string>('None');
+  const [students, setStudents] = useState<string[]>([]);
 
-  const handleDelete = (): void => {};
+  const addStudent = (): void => { };
+
+  const handleEdit = (): void => { };
+
+  const handleDelete = (): void => { };
 
 
   return (
     <div className='card'>
 
-        <div className='cardText'>
-          <h2 className='card-title'>{props.activity}</h2>
-          <p><b>Instructor:</b> {props.instructor}</p>
-          <p><b>Shift:</b> {props.shift}</p>
-          <p><b>Quotas:</b> {props.studentQuotas}</p>
+      <div className='cardText'>
+        <h2 className='card-title'>{props.activity}</h2>
+        <p><b>Instructor:</b> {props.instructor}</p>
+        <p><b>Shift:</b> {props.shift}</p>
+        <p><b>Quotas:</b> {props.studentQuotas}</p>
+      </div>
+
+
+      <div className='cardButtonsContainer'>
+        <div className='addStudent-box'>
+          <Dropdown label='Student' value={selectedStudent} onChange={(value) => setSelectedStudent} name='Add Student' options={[
+            { value: "", label: '' },
+
+          ]} />
+          <Button label='Add' onClick={() => addStudent()} />
         </div>
+        <Button
+          className='edit-button'
+          label="Edit"
+          onClick={() => handleEdit()}
+        />
+        <Button
+          className='delete-button'
+          label="Delete"
+          onClick={() => handleDelete()}
+        />
+      </div>
 
-
-    <div className='cardButtonsContainer'>
-      <Button
-        className='edit-button'
-        label="Edit"
-        onClick={() => handleEdit()}
-      />
-      <Button
-        className='delete-button'
-        label="Delete"
-        onClick={() => handleDelete()}
-      />
-    </div>
-    
 
     </div>
-  ) 
+  )
 }
