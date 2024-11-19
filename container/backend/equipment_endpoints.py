d48cc4266388b58c695fc317d5d1bdfa7617b69d
dutra1243 <facundodutraextra@gmail.com>
2024-11-19 11:24:41 -0300

    final endpoints fix and jsonified

--- container/backend/equipment_endpoints.py
+++ container/backend/equipment_endpoints.py
@@ -1,52 +1,106 @@
 from flask import Blueprint, jsonify, request
-from equipment import Equipment, add_equipment, get_all_equipment, get_equipment_by_activity, get_equipment_by_id, modify_equipment, delete_equipment
+from equipment import (
+    Equipment,
+    add_equipment,
+    get_all_equipment,
+    get_equipment_by_activity,
+    get_equipment_by_id,
+    modify_equipment,
+    delete_equipment,
+)
 
-equipment_bp = Blueprint('equipment', __name__)
+equipment_bp = Blueprint("equipment", __name__)
 
-@equipment_bp.route('/equipment', methods=['POST'])
+
+# working
+@equipment_bp.route("/equipment", methods=["POST"])
 def add_equipment_endpoint():
     data = request.get_json()
     equipment = Equipment(
-        activity_id=data['activity_id'],
-        description=data['description'],
-        cost=data['cost']
+        activity_id=data["activity_id"],
+        description=data["description"],
+        cost=data["cost"],
     )
     result = add_equipment(equipment)
     return jsonify({"message": result}), 201
 
-@equipment_bp.route('/equipment', methods=['GET'])
+
+# working
+# jsonificado
+@equipment_bp.route("/equipment", methods=["GET"])
 def get_all_equipment_endpoint():
     equipment = get_all_equipment()
-    return jsonify(equipment), 200
+    try:
+        equipment = [
+            {
+                "id": equipment[0],
+                "activity_id": equipment[1],
+                "description": equipment[2],
+                "cost": equipment[3],
+            }
+            for equipment in equipment[0]
+        ]
+        return jsonify(equipment), 200
 
-@equipment_bp.route('/equipment/activity/<int:id>', methods=['GET'])
+    except:
+        return jsonify({"message": "No equipment found"}), 404
+
+
+# working
+# jsonificado
+@equipment_bp.route("/equipment/activity/<int:id>", methods=["GET"])
 def get_equipment_by_activity_endpoint(id):
     equipment = get_equipment_by_activity(id)
-    if equipment:
+    try:
+        equipment = [
+            {
+                "id": equipment[0],
+                "activity_id": equipment[1],
+                "description": equipment[2],
+                "cost": equipment[3],
+            }
+            for equipment in equipment[0]
+        ]
         return jsonify(equipment), 200
-    else:
-        return jsonify({"message": "No equipment found for the specified activity"}), 404
+    except:
+        return (
+            jsonify({"message": "No equipment found for the specified activity"}),
+            404,
+        )
 
-@equipment_bp.route('/equipment/<int:id>', methods=['GET'])
+
+# working
+# jsonificado
+@equipment_bp.route("/equipment/<int:id>", methods=["GET"])
 def get_equipment_by_id_endpoint(id):
     equipment = get_equipment_by_id(id)
-    if equipment:
+    try:
+        equipment = {
+            "id": equipment[0][0][0],
+            "activity_id": equipment[0][0][1],
+            "description": equipment[0][0][2],
+            "cost": equipment[0][0][3],
+        }
         return jsonify(equipment), 200
-    else:
+    except:
         return jsonify({"message": "Equipment not found"}), 404
 
-@equipment_bp.route('/equipment/<int:id>', methods=['PUT'])
+
+# working
+@equipment_bp.route("/equipment/<int:id>", methods=["PUT"])
 def modify_equipment_endpoint(id):
     data = request.get_json()
     equipment = Equipment(
-        activity_id=data['activity_id'],
-        description=data['description'],
-        cost=data['cost']
+        activity_id=data["activity_id"],
+        description=data["description"],
+        cost=data["cost"],
     )
     result = modify_equipment(id, equipment)
     return jsonify({"message": result}), 200
 
-@equipment_bp.route('/equipment/<int:id>', methods=['DELETE'])
+
+# working
+@equipment_bp.route("/equipment/<int:id>", methods=["DELETE"])
 def delete_equipment_endpoint(id):
     result = delete_equipment(id)
     return jsonify({"message": result}), 200
