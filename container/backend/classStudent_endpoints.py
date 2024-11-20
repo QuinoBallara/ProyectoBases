e8595de0d442a91f46982187732d6bbac7561c9a
dutra1243 <facundodutraextra@gmail.com>
2024-11-20 16:15:52 -0300

    now a student can't be on more than one class at the same shift

--- container/backend/classStudent_endpoints.py
+++ container/backend/classStudent_endpoints.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, jsonify, request
-from classStudent import ClassStudent, add_class_student, get_class_students
+from classStudent import (
+    ClassStudent,
+    add_class_student,
+    get_class_students,
+    get_classes_on_shift_of_student,
+)
+from classes import get_class_by_id
 
 class_student_bp = Blueprint("class_student", __name__)
 
@@ -21,12 +27,45 @@ def validate_data(data):
     return True
 
 
+def check_for_other_class_on_the_same_shift(student_id):
+    results = get_classes_on_shift_of_student(student_id)
+    formattted_results = [
+        {
+            "class_id": result[0],
+            "dictated": result[1],
+            "instructor_id": result[2],
+            "shift_id": result[3],
+            "activity_id": result[4],
+            "student_quotas": result[5],
+            "shift_id": result[6],
+            "shift_name": result[7],
+            "start_time": f"{result[8]}",
+            "end_time": f"{result[9]}",
+            "equipment_id": result[10],
+            "student_id": result[11],
+        }
+        for result in results[0]
+    ]
+    return formattted_results
+
+
 # working
 @class_student_bp.route("/class-students", methods=["POST"])
 def add_class_student_endpoint():
     data = request.get_json()
+
+    joinedResult = check_for_other_class_on_the_same_shift(data["student_id"])
+    classes = get_class_by_id(data["class_id"])
+    for classJoined in joinedResult:
+        if classJoined["shift_id"] == classes[0][0][3]:
+            return (
+                jsonify({"message": "Student already has a class on this shift"}),
+                403,
+            )
+
     if isinstance(validate_data(data), str):
         return jsonify({"message": validate_data(data)}), 403
+
     class_student = ClassStudent(
         class_id=data["class_id"],
         student_id=data["student_id"],
